6f5674a6e5facd2ba3bb96935e48347727bc3915
Renan dos Santos <44875417+naner777@users.noreply.github.com>
2022-06-24 09:15:27 -0300

    Add files via upload

---
+++ teste.java
@@ -0,0 +1,27 @@
+import java.lang.management.ThreadInfo;
+import java.util.Scanner;
+
+public class teste {
+    public static void main(String[] orgs) {
+       Scanner ent = new Scanner(System.in);
+       integer idade;
+       string sexo;
+
+       System.out.println("digite a idade: ");
+       idade input.nextInt();
+
+       System.out.println("digite o sexo: ");
+       idade input.nextStr();
+
+
+       if ((idade > 18) && (sexo == "m")){
+       System.out.println("true");
+       }
+
+       if ((idade < 18) && (sexo = "f")){
+       
+        result = false;
+        System.out.println("false");
+        }
+    }
+}
